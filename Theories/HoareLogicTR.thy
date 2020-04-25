49438310205635f7460b5b986df8d1e6e5710143
Alfio Martini <alfio.martini@gmail.com>
2020-04-25 01:45:11 -0300

    Applied corrections from RITA2020's Reviewer 2

--- Theories/HoareLogicTR.thy
+++ Theories/HoareLogicTR.thy
@@ -38,21 +38,21 @@ WHILE i<b
   DO p := p * a;i:=i+1 OD
 {p = A^B}"
 
-proof (vcg)
-  fix a b p i 
-  let ?INV = "p = a ^ i \<and> i \<le> b \<and> a = A \<and> b = B"
-  assume ass:"?INV  \<and> i < b"
-  show "p * a = a ^ (i + 1) \<and> i + 1 \<le> b
-        \<and> a = A \<and> b = B"
-    proof - 
-      from ass obtain 1: "p = a ^ i" and 2:"i \<le> b"
-      and 3:"a = A \<and> b = B" and 5:"i<b"  by blast
-      from 1 have 6:"p*a=a^i*a" by simp
-      from this have 7:"p * a = a ^ (i + 1)" by simp
-      from 5 have 8:"i+1\<le>b" by simp
-      from this 3 7 8 show ?thesis by simp
-    qed 
-qed (auto)
+  proof (vcg)
+    fix a b p i 
+    let ?INV = "p = a ^ i \<and> i \<le> b \<and> a = A \<and> b = B"
+    assume ass:"?INV  \<and> i < b"
+    show "p * a = a ^ (i + 1) \<and> i + 1 \<le> b
+          \<and> a = A \<and> b = B"
+      proof - 
+        from ass obtain 1: "p = a ^ i" and 2:"i \<le> b"
+        and 3:"a = A \<and> b = B" and 5:"i<b"  by blast
+        from 1 have 6:"p*a=a^i*a" by simp
+        from this have 7:"p * a = a ^ (i + 1)" by simp
+        from 5 have 8:"i+1\<le>b" by simp
+        from this 3 7 show ?thesis by simp
+      qed 
+  qed (auto)
 
 section \<open>Preliminary Example - List Reversal\<close>
 
@@ -86,8 +86,9 @@ lemma impRev: "VARS (acc::'a list) (xs::'a list)
 apply (vcg) 
 apply (simp)
     using hd_tl_app apply (force)
- apply (auto)
+apply (auto)
 done
+
 
 lemma impRev_isar: "VARS acc x
  {x=X}  acc:=[];
@@ -119,7 +120,7 @@ section \<open>Case Study: Insertion Sort\<close>
 
 fun ins::"'a::linorder \<Rightarrow> 'a list \<Rightarrow> 'a list"
 where  "ins x [] = [x]" |
- "ins x (y # ys) = (if x \<le> y then (x # y # ys)
+ "ins x (y # ys)=(if x \<le> y then (x # y # ys)
                     else y#ins x ys)" 
 
 fun iSort::"('a::linorder) list \<Rightarrow> 'a list" 
@@ -139,6 +140,8 @@ fun count:: "'a \<Rightarrow> 'a list \<Rightarrow> int"  where
   "count x [] = 0" |
   "count x (y # ys) = (if x=y then 
      1 + count x ys else count x ys)"
+
+
 
 fun tail_iSort::"('a::linorder) list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
 "tail_iSort [] acc = acc" |  
@@ -240,20 +243,21 @@ definition is_perm::"'a list \<Rightarrow> 'a list \<Rightarrow> bool"
 where "is_perm l1 l2 \<equiv> length l1 = length l2 
               \<and> (\<forall>x. count x l1 = count x l2)"
 
-lemma inss_hoare: "VARS xs ys :: ('a::linorder) list
- {xs=X}
- ys:=[];
- WHILE xs \<noteq> []
-    INV {isorted ys \<and> is_perm X (ys @ xs)}
- DO ys := ins (hd xs) ys; xs := tl xs OD
-{isorted ys \<and> is_perm X ys}"
- apply (vcg)  
- apply (auto simp add:is_perm_def) \<comment> \<open> 1 \<close>
-   apply (simp add: ins_sorted) \<comment> \<open>1.1\<close>
-   apply (simp add: ins_len) \<comment> \<open>1.2\<close>
-   apply  (smt count.simps(2) count_sum ins_count 
-           list.collapse) \<comment>\<open>1.3\<close>
-done
+  lemma inss_hoare:
+    "VARS xs ys :: ('a::linorder) list
+   {xs=X}
+   ys:=[];
+   WHILE xs \<noteq> []
+      INV {isorted ys \<and> is_perm X (ys @ xs)}
+   DO ys := ins (hd xs) ys; xs := tl xs OD
+  {isorted ys \<and> is_perm X ys}"
+   apply (vcg)  
+   apply (auto simp add:is_perm_def) \<comment> \<open> 1 \<close>
+     apply (simp add: ins_sorted) \<comment> \<open>1.1\<close>
+     apply (simp add: ins_len) \<comment> \<open>1.2\<close>
+     apply  (smt count.simps(2) count_sum 
+            ins_count list.collapse) \<comment>\<open>1.3\<close>
+  done
      
 
 
@@ -281,27 +285,30 @@ lemma inss_isar_draft:
 "VARS xs ys :: ('a::linorder) list
  {xs=X} ys:=[];
  WHILE xs \<noteq> []
-       INV {isorted ys \<and> is_perm X (ys @ xs)}
+    INV {isorted ys \<and> is_perm X (ys @ xs)}
  DO ys := ins (hd xs) ys; xs := tl xs OD
 {isorted ys \<and> is_perm X ys}"
-
-    proof (vcg)
-       fix xs ys 
-       assume ass:"(isorted ys \<and> 
-          is_perm X (ys @ xs)) \<and> xs \<noteq> []"   
-       show "isorted (ins (hd xs) ys) 
-              \<and> is_perm X ((ins (hd xs) ys) @ tl xs)" 
-       proof (rule conjI)
-           show "isorted (ins (hd xs) ys)" sorry
-       next
-           have pg1:"length X  =  
-            length ((ins (hd xs) ys) @ tl xs)" sorry 
-           have pg2:"\<forall> k. count k X = 
-            count k (ins (hd xs) ys @ tl xs)"  sorry
-           from pg1 pg2 show 
-            "is_perm X (ins (hd xs) ys @ tl xs)" sorry 
-       qed
-    qed (auto simp add:is_perm_def)
+proof (vcg)
+   fix xs ys 
+   assume ass:"(isorted ys \<and> 
+      is_perm X (ys @ xs)) \<and> xs \<noteq> []"   
+   show "isorted (ins (hd xs) ys) 
+     \<and> is_perm X ((ins (hd xs) ys) @ tl xs)" 
+   proof (rule conjI)
+     show "isorted (ins (hd xs) ys)" 
+        sorry
+   next
+       have pg1:"length X  =  
+        length ((ins (hd xs) ys) @ tl xs)"
+           sorry 
+       have pg2:"\<forall> k. count k X = 
+        count k (ins (hd xs) ys @ tl xs)"  
+           sorry
+       from pg1 pg2 show 
+        "is_perm X (ins (hd xs) ys @ tl xs)"
+           sorry 
+   qed
+qed (auto simp add:is_perm_def)
 
 
 lemma inss_isar: "VARS xs ys :: ('a::linorder) list
@@ -311,78 +318,78 @@ lemma inss_isar: "VARS xs ys :: ('a::linorder) list
        INV {isorted ys \<and> is_perm X (ys @ xs)}
        DO ys := ins (hd xs) ys; xs := tl xs OD
        {isorted ys \<and> is_perm X ys}"
-
 proof (vcg)
-   fix xs ys 
-   assume ass:"(isorted ys \<and> is_perm X (ys @ xs))
-                 \<and> xs \<noteq> []"   
-   show "isorted (ins (hd xs) ys)
-        \<and> is_perm X ((ins (hd xs) ys) @ tl xs)" 
-      proof (rule conjI)
-         from ass have "isorted ys" by simp
-         from this show "isorted (ins (hd xs) ys)" 
-            by (simp add:ins_sorted)
-      next 
-        from ass have 1:"is_perm X (ys @ xs)" 
-           and 2:"xs \<noteq> []" by auto 
-        from 2 have hdtl:"xs = hd xs # tl xs" by simp 
-        from 1 have 3:"\<forall> x. count x X = count x (ys @ xs)" 
-                  by (simp add:is_perm_def)
-        have pg1:"length X 
-              =  length ((ins (hd xs) ys) @ tl xs)"
-         proof - 
-           from 1 have 4:"length X = length (xs @ ys)" 
-                 by (simp add:is_perm_def)
-           also have "\<dots>= length xs + length ys" by simp
-           also have "\<dots> =  1 + length ys + 
-                   length xs - 1" by simp
-           also have "\<dots> = length (ins (hd xs) ys) + 
-                     length (tl xs)"
-                            by (simp add: "2" ins_len)
-           also have "\<dots> = 
-             length ((ins (hd xs) ys) @ tl xs)" by simp
-           finally show ?thesis  by simp
-         qed 
-        have pg2:"\<forall> k. count k X = count k (ins (hd xs) ys @ tl xs)" 
-           proof (rule allI)
-             fix k
-             have "count k X = count k ((ins (hd xs) ys) @ tl xs)"
-                proof (cases "k= hd xs")
-                   assume case1: "k = hd xs" 
-                   have "count k ((ins (hd xs) ys) @ tl xs) = 
-                         count k (ins (hd xs) ys) + count k (tl xs)"  
-                             by (simp add:count_sum)
-                   also have "\<dots> = 1 + count k ys + count k (tl xs)" 
-                       by (simp add:case1 ins_count)
-                   also have "\<dots> =  count k ys + 1 + count k (tl xs)" by simp
-                   also have "\<dots>= count k ys + count k ((hd xs) # tl xs)" 
-                        by (simp add: case1) 
-                   also have "\<dots>= count k ys + count k xs"  using hdtl by auto 
-                   also have "\<dots>= count k (ys @ xs)" by (simp add:count_sum)
-                   also have "\<dots>= count k X" by (simp add: "3")
-                   finally show "count k X = count k ((ins (hd xs) ys) @ tl xs)"
-                      by simp
-                next 
-                   assume case2: "k \<noteq> hd xs" 
-                    have "count k ((ins (hd xs) ys) @ tl xs) = 
-                         count k (ins (hd xs) ys) + count k (tl xs)"  
-                             by (simp add:count_sum)
-                   also have "\<dots> = count k ys + count k (tl xs)" 
-                       by (simp add:case2 ins_count)
-                   also have "\<dots>= count k ys + count k ((hd xs) # tl xs)" 
-                        by (simp add: case2) 
-                   also have "\<dots>= count k ys + count k xs"  using hdtl by auto 
-                   also have "\<dots>= count k (ys @ xs)" by (simp add:count_sum)
-                   also have "\<dots>= count k X" by (simp add: "3")
-                   finally show "count k X = count k ((ins (hd xs) ys) @ tl xs)"
-                      by simp
-                qed
-                from this show "count k X = count k (ins (hd xs) ys @ tl xs)"
-                  by assumption
-             qed
-        from pg1 pg2 show "is_perm X (ins (hd xs) ys @ tl xs)" 
+ fix xs ys 
+ assume ass:"(isorted ys \<and> is_perm X (ys @ xs))
+               \<and> xs \<noteq> []"   
+ show "isorted (ins (hd xs) ys)
+      \<and> is_perm X ((ins (hd xs) ys) @ tl xs)" 
+  proof (rule conjI)
+     from ass have "isorted ys" by simp
+     from this show "isorted (ins (hd xs) ys)" 
+        by (simp add:ins_sorted)
+  next 
+    from ass have 1:"is_perm X (ys @ xs)" 
+       and 2:"xs \<noteq> []" by auto 
+    from 2 have hdtl:"xs = hd xs # tl xs" by simp 
+    from 1 have 
+         3:"\<forall> x. count x X = count x (ys @ xs)" 
              by (simp add:is_perm_def)
-      qed
+    have pg1:"length X 
+          =  length ((ins (hd xs) ys) @ tl xs)"
+     proof - 
+       from 1 have 4:"length X = length (xs @ ys)" 
+             by (simp add:is_perm_def)
+       also have "\<dots>= length xs + length ys" by simp
+       also have "\<dots> =  1 + length ys + 
+               length xs - 1" by simp
+       also have "\<dots> = length (ins (hd xs) ys) + 
+                 length (tl xs)"
+                        by (simp add: "2" ins_len)
+       also have "\<dots> = 
+         length ((ins (hd xs) ys) @ tl xs)" by simp
+       finally show ?thesis  by simp
+     qed 
+    have pg2:"\<forall> k. count k X = count k (ins (hd xs) ys @ tl xs)" 
+       proof (rule allI)
+         fix k
+         have "count k X = count k ((ins (hd xs) ys) @ tl xs)"
+            proof (cases "k= hd xs")
+               assume case1: "k = hd xs" 
+               have "count k ((ins (hd xs) ys) @ tl xs) = 
+                     count k (ins (hd xs) ys) + count k (tl xs)"  
+                         by (simp add:count_sum)
+               also have "\<dots> = 1 + count k ys + count k (tl xs)" 
+                   by (simp add:case1 ins_count)
+               also have "\<dots> =  count k ys + 1 + count k (tl xs)" by simp
+               also have "\<dots>= count k ys + count k ((hd xs) # tl xs)" 
+                    by (simp add: case1) 
+               also have "\<dots>= count k ys + count k xs"  using hdtl by auto 
+               also have "\<dots>= count k (ys @ xs)" by (simp add:count_sum)
+               also have "\<dots>= count k X" by (simp add: "3")
+               finally show "count k X = count k ((ins (hd xs) ys) @ tl xs)"
+                  by simp
+            next 
+               assume case2: "k \<noteq> hd xs" 
+                have "count k ((ins (hd xs) ys) @ tl xs) = 
+                     count k (ins (hd xs) ys) + count k (tl xs)"  
+                         by (simp add:count_sum)
+               also have "\<dots> = count k ys + count k (tl xs)" 
+                   by (simp add:case2 ins_count)
+               also have "\<dots>= count k ys + count k ((hd xs) # tl xs)" 
+                    by (simp add: case2) 
+               also have "\<dots>= count k ys + count k xs"  using hdtl by auto 
+               also have "\<dots>= count k (ys @ xs)" by (simp add:count_sum)
+               also have "\<dots>= count k X" by (simp add: "3")
+               finally show "count k X = count k ((ins (hd xs) ys) @ tl xs)"
+                  by simp
+            qed
+            from this show "count k X = count k (ins (hd xs) ys @ tl xs)"
+              by assumption
+         qed
+    from pg1 pg2 show "is_perm X (ins (hd xs) ys @ tl xs)" 
+         by (simp add:is_perm_def)
+  qed
 qed (auto simp add:is_perm_def)
 
 end
